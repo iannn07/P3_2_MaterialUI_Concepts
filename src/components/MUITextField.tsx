3991da6ce3f85d644685b82f16563f25b5f458ea
Iannn <pristianb007@gmail.com>
2024-03-07 08:58:01 +0700

    A lil bit adjustment

--- src/components/MUITextField.tsx
+++ src/components/MUITextField.tsx
@@ -1,6 +1,8 @@
 import { Stack, TextField } from "@mui/material";
+import { useState } from "react";
 
 const MUITextField = () => {
+  const [values, setValues] = useState("");
   return (
     <Stack spacing={2}>
       <Stack direction="row" spacing={2}>
@@ -9,8 +11,19 @@ const MUITextField = () => {
         <TextField label="Fill Here" variant="standard" />
       </Stack>
       <Stack direction="row" spacing={2}>
-        <TextField label="Required" required />
+        <TextField
+          label="Required"
+          required
+          error={values.length <= 4 && values.length !== 0}
+          helperText={values.length <= 4 && values.length !== 0 ? "Minimum 5 Characters" : " "}
+          value={values}
+          onChange={(e) => setValues(e.target.value)}
+        />
         <TextField label="Password" type="password" />
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <TextField label="Amount" InputProps={{ startAdornment: "$" }} />
+        <TextField label="Weight" InputProps={{ endAdornment: "kg" }} />
       </Stack>
     </Stack>
   );
